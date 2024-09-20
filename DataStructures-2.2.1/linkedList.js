b506c89a8735b99503013933ae6208563cb53085
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2024-09-19 21:29:53 -0600

    feat: linked lists

---
+++ DataStructures-2.2.1/linkedList.js
@@ -0,0 +1,35 @@
+class LinkedListNode {
+    constructor(data,next=null){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor(head = new LinkedListNode("Dummy")){
+        this.head = head;
+    }
+
+    appendNode(data){
+        let currentNode = this.head;
+        while(currentNode.next){
+            currentNode = currentNode.next;
+        }
+        currentNode.next = new LinkedListNode(data);
+    }
+}
+let head = new LinkedListNode(7)
+let myLinkedList = new LinkedList(head);
+myLinkedList.appendNode(5);
+myLinkedList.appendNode(8);
+myLinkedList.appendNode(10);
+
+
+let currentNode = myLinkedList.head;
+let sumaDeNodos = 0;
+while(currentNode){
+    sumaDeNodos += currentNode.data;
+    currentNode = currentNode.next;
+}
+
+console.log(sumaDeNodos);
